6bbd955f54f9bbc0ff7cfab036682a0944b3baac
dominikus1993 <dominikus1910@gmail.com>
2021-02-08 20:17:39 +0100

    small changes

--- core/model/model.go
+++ core/model/model.go
@@ -1,1 +1,25 @@
 package model
+
+import "github.com/streadway/amqp"
+
+func a(rabbitmq *amqp.Connection) {
+	ch, _ := rabbitmq.Channel()
+
+	msgs, _ := ch.Consume(
+		"q.Name",  // queue
+		"crawler", // consumer
+		true,      // auto-ack
+		false,     // exclusive
+		false,     // no-local
+		false,     // no-wait
+		nil,       // args
+	)
+	for msg := range msgs {
+		msg.
+	}
+
+}
+
+type HossipeMessage struct {
+	Body []byte
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module easynetq-hossipe
 
 go 1.15
+
+require (
+	github.com/gocolly/colly/v2 v2.1.0
+	github.com/streadway/amqp v1.0.0
+)
