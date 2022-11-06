96d9a9725405a89bd26a80cfda1704f5baf546e8
Dominik.Kotecki <dominikus1910@gmail.com>
2022-11-06 16:50:48 +0100

    init csharp

--- pkg/config/cfg.go
+++
@@ -1,9 +0,0 @@
-package config
-
-type ErrorMessageSubscriberConfig struct {
-	Queue string
-}
-
-func NewErrorMessageSubscriberConfig(queue string) *ErrorMessageSubscriberConfig {
-	return &ErrorMessageSubscriberConfig{Queue: queue}
-}
