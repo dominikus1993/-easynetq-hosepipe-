d172e2dd81d740ae1a1ac57ce1cfaf72958121c1
Dominik.Kotecki <dominikus1910@gmail.com>
2022-11-09 19:43:36 +0100

    net 7

---
+++ src/EasyNetQ.HosePipe/Infrastructure/Subscriber.cs
@@ -0,0 +1,9 @@
+namespace EasyNetQ.HosePipe.Infrastructure;
+
+public class Subscriber : BackgroundService
+{
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        throw new NotImplementedException();
+    }
+}
